5ae38d3dbb64a8b6867f9e2a0b5eb1cccbe3938a
Colin Armstrong <colinwarmstrong@gmail.com>
2018-09-12 11:13:27 -0600

    Add unit test for the user instance method #score.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,21 @@ describe User, type: :model do
   context 'Validations' do
     it { should have_many :plays }
   end
+
+  context 'Instance Methods' do
+    it '#score' do
+      josh = User.create(id: 1, name: "Josh")
+      sal = User.create(id: 2, name: "Sal")
+
+      game = Game.create(player_1: josh, player_2: sal)
+
+      josh.plays.create(game: game, word: "sal", score: 3)
+      josh.plays.create(game: game, word: "zoo", score: 12)
+      sal.plays.create(game: game, word: "josh", score: 14)
+      sal.plays.create(game: game, word: "no", score: 2)
+
+      expect(josh.score).to eq(15)
+      expect(sal.score).to eq(16)
+    end
+  end
 end
