793fb805b5fe7b921682869ace605b6969c9b0b1
Colin Armstrong <colinwarmstrong@gmail.com>
2018-09-12 11:56:05 -0600

    Add presenter spec to test the word validation presenter returns the correct messages to the view.

---
+++ spec/presenters/word_validation_presenter_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe WordValidationPresenter do
+  context '#word_validation' do
+    it 'returns the correct message when given a valid word', :vcr do
+      word = 'foxes'
+
+      presenter = WordValidationPresenter.new(word)
+
+      message = presenter.word_validation
+
+      expect(message).to eq("'foxes' is a valid word and its root form is 'fox'.")
+    end
+
+    it 'returns the correct message when given an invalid word', :vcr do
+      word = 'foxez'
+
+      presenter = WordValidationPresenter.new(word)
+
+      message = presenter.word_validation
+
+      expect(message).to eq("'foxez' is not a valid word.")
+    end
+  end
+end
--- spec/services/ofxford_dictionary_service_spec.rb
+++ spec/services/ofxford_dictionary_service_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 describe OxfordDictionaryService do
-  it 'returns a 200 status when given a valid word', :vcr do
-    service = OxfordDictionaryService.new
+  context '#word_information' do
+    it 'returns a 200 status when given a valid word', :vcr do
+      service = OxfordDictionaryService.new
 
-    word = 'foxes'
+      word = 'foxes'
 
-    response = service.word_information(word)
+      response = service.word_information(word)
 
-    expect(response.status).to eq(200)
-  end
+      expect(response.status).to eq(200)
+    end
 
-  it 'returns a 404 status when given an invalid word', :vcr do
-    service = OxfordDictionaryService.new
+    it 'returns a 404 status when given an invalid word', :vcr do
+      service = OxfordDictionaryService.new
 
-    word = 'foxez'
+      word = 'foxez'
 
-    response = service.word_information(word)
+      response = service.word_information(word)
 
-    expect(response.status).to eq(404)
+      expect(response.status).to eq(404)
+    end
   end
 end
