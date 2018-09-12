50704346597cff63103d89cc63f3f91a7492c47b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-09-12 11:02:16 -0600

    Pulled most of the logic in the create plays method into private methods.

--- app/controllers/api/v1/games/plays_controller.rb
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::Games::PlaysController < ApplicationController
   def create
-    game = Game.find(game_params[:game_id])
-    play = game.plays.create(user_id: game_params[:user_id], word: game_params[:word])
-
     render json: play, status: 201
   end
 
@@ -10,5 +7,13 @@ class Api::V1::Games::PlaysController < ApplicationController
 
   def game_params
     params.permit(:game_id, :user_id, :word)
+  end
+
+  def play
+    game.plays.create(user_id: game_params[:user_id], word: game_params[:word])
+  end
+
+  def game
+    Game.find(game_params[:game_id])
   end
 end
