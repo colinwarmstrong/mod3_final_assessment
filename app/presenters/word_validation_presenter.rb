10e586b61e576133428531d215db601c6d0cf05b
Colin Armstrong <colinwarmstrong@gmail.com>
2018-09-12 11:20:24 -0600

    Pulled unnecessary logic out of the service and into the presenter.

--- app/presenters/word_validation_presenter.rb
+++ app/presenters/word_validation_presenter.rb
@@ -4,6 +4,21 @@ class WordValidationPresenter
   end
 
   def word_validation
-    OxfordDictionaryService.new.validate_word(@word)
+    if response.status == 200
+      word_information = JSON.parse(response.body, symbolize_names: true)
+      message(word_information)
+    else
+      "'#{@word}' is not a valid word."
+    end
+  end
+
+  private
+
+  def response
+    OxfordDictionaryService.new.word_information(@word)
+  end
+
+  def message(word_info)
+    "'#{word_info[:results][0][:word]}' is a valid word and its root form is '#{word_info[:results][0][:lexicalEntries][0][:inflectionOf][0][:id]}'."
   end
 end
--- app/service/oxford_dictionary_service.rb
+++ app/service/oxford_dictionary_service.rb
@@ -1,30 +1,19 @@
 class OxfordDictionaryService
   def initialize(source_language = 'en')
-    @conn = conn
     @source_language = source_language
   end
 
-  def validate_word(word = 'foxes')
-   response = conn.get("inflections/#{@source_language}/#{word}")
-    if response.status == 200
-      word_information = JSON.parse(response.body, symbolize_names: true)
-      message(word_information)
-    else
-      "'#{word}' is not a valid word."
-    end
+  def word_information(word)
+    conn.get("inflections/#{@source_language}/#{word}")
   end
 
   private
 
   def conn
     Faraday.new(url: 'https://od-api.oxforddictionaries.com/api/v1/') do |faraday|
-      faraday.headers['app_id'] = ENV['OXFORD_APP_ID']
+      faraday.headers['app_id']  = ENV['OXFORD_APP_ID']
       faraday.headers['app_key'] = ENV['OXFORD_APP_KEY']
       faraday.adapter Faraday.default_adapter
     end
-  end
-
-  def message(word_info)
-    "'#{word_info[:results][0][:word]}' is a valid word and its root form is '#{word_info[:results][0][:lexicalEntries][0][:inflectionOf][0][:id]}'."
   end
 end
